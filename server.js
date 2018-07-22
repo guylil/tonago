681910c32b3e96179976685442614e3a03568f4d
G <guylilian@gmail.com>
2018-07-22 15:17:04 +0300

    deploying app

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const books = require('./assets/books.json');
+const path = require('path');
+const serveStatic = require('serve-static');
+const PORT = process.env.PORT || 3000;
 
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.get('/', serveStatic('./public/', {'index': 'index.html'}));
 
 app.get('/books/v1/volumes/', function(req, res) {
    // console.log('req: ', req.query);
@@ -20,4 +26,4 @@ app.get('/books/v1/volumes/', function(req, res) {
    });
 });
 
-app.listen(3000, () => console.log('Server is listening at 3000'));
+app.listen(PORT, () => console.log('Server is listening at ', PORT));
