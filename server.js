84335b1ad92bcf402bb978cb7ce2540f5d46e8c2
G <guylilian@gmail.com>
2018-07-22 16:30:02 +0300

    port changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const books = require('./assets/books.json');
 const path = require('path');
 const serveStatic = require('serve-static');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
